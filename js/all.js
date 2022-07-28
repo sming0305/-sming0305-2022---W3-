040d11f27b3445d79b66d372bb1afe9e0e89b377
YI <sming0305@gmail.com>
2022-07-28 22:09:52 +0800

    2022-W3 完成

--- js/all.js
+++ js/all.js
@@ -1,11 +1,29 @@
 $(document).ready(function () {
-    $(".nav-mobile-menu").click(function (e) { 
+    $(".nav-mobile-menu").click(function (e) {
         e.preventDefault();
         $(".wrap").toggleClass("open");
     });
-    $(".close").click(function (e) { 
+    $(".close").click(function (e) {
         e.preventDefault();
         $(".wrap").removeClass("open");
     });
+
+    $(".goTop").click(function (e) {
+        e.preventDefault();
+        $("html,body").animate(
+            {
+                scrollTop: 0
+            }, 1000
+        );
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 500) {
+            $(".goTop").fadeIn(700);
+        } else {
+            $(".goTop").fadeOut(700);
+        }
+    });
+
 });
 
